7997fdb76021873f8149b0b19ef621121bd6b2ac
Heather Nye <heatherdnye@gmail.com>
2019-11-05 16:33:59 -0600

    github-usercard

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,22 +2,29 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
- axios.get('https://api.github.com/users/hdnye') 
 
-        .then((response) => {
-          console.log(response);
+const cards = document.querySelector('.cards');
 
-          response.data.user_url.forEach((card) => {
-          const userData = new userCard(card); 
-         
-          cards.appendChild(userData);
-         });
+ axios
+ 
+ .get('https://api.github.com/users/hdnye') 
 
-       })
+        .then(response => {
 
-       .catch((error) => {
+          const cardNew = githubCard(response);
+
+          cards.appendChild(cardNew);
+
+          return cards;
+        })
+        
+
+         .catch((error) => {
       console.log(error);
- });
+     });
+
+ 
+
 /*/
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -33,14 +40,13 @@
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
+          at the bottom of the page. Get at least 5 different Github userNames and add them as
           Individual strings to the friendsArray below.
           
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -49,7 +55,7 @@ const followersArray = [];
   <img src={image url of user} />
   <div class="card-info">
     <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
+    <p class="userName">{users user name}</p>
     <p>Location: {users location}</p>
     <p>Profile:  
       <a href={address to users github page}>{address to users github page}</a>
@@ -61,77 +67,93 @@ const followersArray = [];
 </div>
 
 */
-function githubCard(userData){
+ 
+const followersArray = axios;
 
-  const userCard = document.createElement('div');
+
+axios
+  .get('https://api.github.com/users/hdnye/followers')
+  .then(response => {
+   
+      response.data.forEach(followers => 
+
+        axios.get('https://api.github.com/users/hdnye/${followers.login}')
+
+        .then(response => { 
+        
+        return cards.appendChild(githubCard(response.data));
+    })
+    .catch(error => console.log(error))
+      );
+  })
+     .catch(error => console.log(error));
+
+  
+
+console.log(followersArray);
+
+let githubCard = (data) => {
+
+  const card = document.createElement('div');
   const userImage = document.createElement('img');
-  const userInfo = document.createElement('div');
-  const githubName = document.createElement('h3');
-  const userName = document.createElement('p');  
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
   const userLocation = document.createElement('p');
-  const userProfile = document.createElement('p');
+  const userProfile = document.createElement('p'); 
+  const a = document.createElement('a')   
   const userFollowers = document.createElement('p');
   const userFollowing = document.createElement('p');
   const userBio = document.createElement('p');
+ 
+  card.appendChild(userImage);
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(userLocation);
+  cardInfo.appendChild(userProfile);
+  cardInfo.appendChild(a);
+  cardInfo.appendChild(userFollowers);
+  cardInfo.appendChild(userFollowing);
+  cardInfo.appendChild(userBio);
 
 
-  userCard.classList.add('card');
-  userImage.src = imgSrc;
-  userInfo.classList.add('card-info');
-  githubName.classList.add('name');
-  userName.classList.add('username');
+  card.classList.add('card');
+  userImage.classList.add('img');
+  cardInfo.classList.add('card-info');
+  name.classList.add('card-info');
+  userName.classList.add('card-info');
+  userLocation.classList.add('card-info');
+  userProfile.classList.add('card-info');
+  a.classList.add('card-info');
+  userFollowers.classList.add('card-info');
+  userFollowing.classList.add('card-info');
+  userBio.classList.add('card-info');
 
-  userData.forEach( (item) => {  
-    
-    container.appendChild(githubCard(item));
+ userImage.src = data.avatar_url;
+ name.textContent = data.name;
+ userName.textContent = data.login;
+ userLocation.textContent = data.location;
+ a.link = data.html_url;
+ userFollowers.textContent = data.followers;
+ userFollowing.textContent = data.following;
+ userBio.textContent = data.bio;
+ 
+return card;
   
-  })
-
-
-  userInfo.appendChild(githubName);
-  userInfo.appendChild(userName);
-  userInfo.appendChild(userLocation);
-  userInfo.appendChild(userProfile);
-  userInfo.appendChild(userFollowers);
-  userInfo.appendChild(userFollowing);
-  userInfo.appendChild(userBio);
-  
-  userCard.appendChild(userImage);
-  userCard.appendChild(userInfo);
-
-  return githubCard;
 } 
 
-console.log(githubCard(userData[i]));
-console.log(githubCard(followersArray[i]));
 
-axios.post('https://api.github.com/users/hdnye/followers')
-  .then((response) => {
-  console.log(response);
-
-   response.data.followers_url.forEach((card) => {
-    const newEntry = new FriendsCard(card); 
- 
-   cards.appendChild(newEntry);
-  });
-
-})
-
-.catch((error) => {
-console.log(error);
-});
-
-
-
-/* List of LS Instructors Github username's: 
+/* List of LS Instructors Github userName's: 
   tetondan
   dustinmyers
   justsml
   luishrd
   bigknell
-*/
 
-/*--Class Notes--
+
+
+--Class Notes--
 
 create, read, update and delete: CRUD App (ex: fb, twitter)
 
@@ -186,8 +208,5 @@ ex:
   })
     .catch((err) => {
       console.log(err);
-});
-
-
-
+}); 
 */
